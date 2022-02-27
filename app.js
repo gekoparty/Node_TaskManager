b5fe86095d26aca0dfd6075a42582dc4c6c50f80
gekoparty <gekoparty@hotmail.com>
2022-02-27 22:20:47 +0100

    Routes and controller basic

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-
 const port = 3000;
+const tasks = require('./routes/tasks')
+
+
+//middleware
+app.use(express.json());
 
 //routes
+app.get("/hello", (req, res) => {
+  res.send("Task Manager App");
+});
 
-app.get('/hello', (req,res)=> {
-    res.send('Task Manager App')
-})
+app.use('/api/v1/tasks', tasks)
 
-app.listen(port, console.log(`the server is listening on port ${port}`))
+app.listen(port, console.log(`the server is listening on port ${port}`));
 
-console.log('Task Manager App')
+console.log("Task Manager App");
